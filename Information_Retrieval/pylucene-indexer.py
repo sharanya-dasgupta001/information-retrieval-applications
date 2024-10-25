8da3b0e65bafe9cc5cc8375d42b2f6494b0e2dc3
Sharanya Dasgupta <69811107+sharanya-dasgupta001@users.noreply.github.com>
2024-10-25 13:10:33 +0530

    Add files via upload

---
+++ Information_Retrieval/pylucene-indexer.py
@@ -0,0 +1,44 @@
+file_path = 'wiki_movie_plots_deduped.csv'
+
+# Essential imports:
+import pandas as pd
+import lucene
+from java.io import File
+
+# Indexer imports:
+from org.apache.lucene.analysis.standard import StandardAnalyzer
+from org.apache.lucene.analysis.en import EnglishAnalyzer
+from org.apache.lucene.index import IndexWriter, IndexWriterConfig
+#from org.apache.lucene.store import SimpleFSDirectory, FSDirectory
+from org.apache.lucene.store import FSDirectory
+import org.apache.lucene.document as document
+
+lucene.initVM()
+
+indexPath = File("index-2/").toPath()
+indexDir = FSDirectory.open(indexPath)
+
+writerConfig = IndexWriterConfig(EnglishAnalyzer())
+writer = IndexWriter(indexDir, writerConfig)
+
+def indexMovie(title, plot):
+    doc = document.Document()
+    doc.add(document.Field("TITLE", title, document.TextField.TYPE_STORED))
+    doc.add(document.Field("PLOT", plot, document.TextField.TYPE_STORED))
+    writer.addDocument(doc)
+
+def closeWriter():
+    writer.close()
+
+def makeIndex(file_path):
+    df = pd.read_csv(file_path)
+    
+    docid = 0
+    for i in df.index:
+        print(docid, "-", df['Title'][i])
+        indexMovie(df['Title'][i], df['Plot'][i])
+        docid += 1
+
+makeIndex(file_path)
+closeWriter()
+
